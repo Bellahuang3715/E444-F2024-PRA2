6cbaab08757c34738954726e6626ed917dba29a5
Bella Huang <bella.huang@yourenext.ca>
2024-09-22 14:05:08 -0400

    create custom error page and update user.html to use base template

--- hello.py
+++ hello.py
@@ -4,6 +4,7 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -13,3 +14,12 @@ def index():
 def user(name):
     return render_template('user.html', name=name)
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
