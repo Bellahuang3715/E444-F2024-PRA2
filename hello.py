11398e67bef94ee304bd6e4673f9b9548beca1e4
Bella Huang <bella.huang@yourenext.ca>
2024-09-26 11:44:50 -0400

    create email field and a custom validator for the field

--- hello.py
+++ hello.py
@@ -5,10 +5,13 @@ from datetime import datetime
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
@@ -17,14 +20,27 @@ moment = Moment(app)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
+    # session['name'] = None
+    # session['email'] = None
+    
     if form.validate_on_submit():
+        # configure user name
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+        
+        # configure user email
+        old_email = session.get('email')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        session['email'] = form.email.data        
+        
         return redirect(url_for('index'))
-    return render_template('index.html',
-        form = form, name = session.get('name'))
+    return render_template('index.html', 
+                           form=form,
+                           name=session.get('name'),
+                           email=session.get('email'))
 
 
 @app.route('/user/<name>')
@@ -41,7 +57,15 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-
+    
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email address?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+    
+    # function name format for custom validators: validate_<fieldname> 
+    def validate_email(self, field):
+        if 'utoronto' not in field.data:
+            raise ValidationError("Please provide a UofT email address.")
+        elif '@' not in field.data:
+            raise ValidationError(f'Please include an "@" in the email address. {field.data} is missing an "@".')
